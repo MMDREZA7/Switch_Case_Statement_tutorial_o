6b576d2119debdc41874e54a183f4a0274643511
MMDREZA <mheidari831003.game@gmail.com>
2024-02-17 14:57:41 +0330

    tutorial finished

---
+++ switch_case.dart
@@ -0,0 +1,59 @@
+void main() {
+  String person = 'hello';
+
+  switch (person) {
+    case 'woman':
+      print(person + ": She is a female");
+      break;
+
+    case 'girl':
+      print(person + ": She is a female");
+      break;
+
+    case 'man':
+      print(person + ": He is a male");
+      break;
+
+    case 'boy':
+      print(person + ": He is a male");
+      break;
+
+    default:
+      print("How Are You?");
+      break;
+  }
+
+  // switch (number) {
+  //   case 0:
+  //     today = "Sunday";
+  //     break;
+
+  //   case 1:
+  //     today = "Monday";
+  //     break;
+
+  //   case 2:
+  //     today = "Tuesday";
+  //     break;
+
+  //   case 3:
+  //     today = "Wednesday";
+  //     break;
+
+  //   case 4:
+  //     today = "Thursday";
+  //     break;
+
+  //   case 5:
+  //     today = "Friday";
+  //     break;
+
+  //   case 6:
+  //     today = "Saturday";
+  //     break;
+
+  //   default:
+  //     today = "unknown";
+  // }
+  // print(today);
+}
